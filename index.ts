831aa476773ed1dff2bc5af84a743d32afeaa3b1
Anton Galtsev <avgaltsev@gmail.com>
2018-03-13 00:40:28 +0300

    Rethinked

--- index.ts
+++ index.ts
@@ -1,143 +1,133 @@
 import * as path from "path";
 
 import * as postcss from "postcss";
-import {Transformer, plugin} from "postcss";
+import {plugin, Transformer, Rule, Declaration, AtRule} from "postcss";
 
 import * as packageJson from "./package.json";
 
 const POSTCSS_PLUGIN: string = packageJson.name;
 const POSTCSS_VERSION: string = packageJson.version;
 
-const SCOPE_PATTERN = /^(?:[a-zA-Z_][a-zA-Z0-9_-]*)?$/;
+const KEYFRAMES_ATRULE_NAME_PATTERN = /^(?:-[a-z]-)?(?:keyframes)$/;
 
 const CLASS_PATTERN = /\.([a-zA-Z_][a-zA-Z0-9_-]*)\b(?!:(?:mod|global))/g;
 const MOD_CLASS_PATTERN = /\.([a-zA-Z_][a-zA-Z0-9_-]*):mod\((["'])?([a-zA-Z_][a-zA-Z0-9_-]*)\2\)/g;
 const GLOBAL_CLASS_PATTERN = /\.([a-zA-Z_][a-zA-Z0-9_-]*):global\b/g;
 
+const ANIMATION_DECLARATION_NAME_PATTERN = /^(?:-[a-z]-)?(?:animation|animation-name)$/;
+const ANIMATION_DECLARATION_TOKEN_PATTERN = /(?:^|\s)(?:[+-]?\d+(?:s|ms)?|linear|ease|ease-in|ease-out|ease-in-out|cubic-bezier\([^)]+\)|step-start|step-end|steps\([^)]+\)|frames\([^)]+\)|infinite|normal|reverse|alternate|alternate-reverse|none|forwards|backwards|both|running|paused)(?=$|\s)/g;
+
+export interface NameGenerator {
+	(filePath: string, name: string, modName: string): string;
+}
+
+export interface ScopeTaker {
+	(scope: Scope): void;
+}
+
+export interface PluginOptions {
+	generateName?: NameGenerator;
+	takeScope?: ScopeTaker;
+}
+
 export interface Scope {
 	[name: string]: {
-		base?: string;
+		base: string;
 		mods?: {
 			[name: string]: string;
 		};
 	};
 }
 
-export interface Scopes {
-	[name: string]: Scope;
-}
+const generateDefaultName: NameGenerator = (filePath, name, modName?) => {
+	const fileName = filePath ? path.normalize(path.dirname(filePath) + path.sep + path.basename(filePath, path.extname(filePath))).split(path.sep) : [];
 
-export interface Exports {
-	scopes: Scopes;
-	defaultScope: Scope;
-}
+	return [...fileName, name, ...(modName ? [modName] : [])].join("_");
+};
 
-export interface Cache {
-	[name: string]: string;
-}
+const dontTakeScope: ScopeTaker = (scope) => {};
 
-export interface Context {
-	exports: Exports;
-	cache: Cache;
-}
+class Processor {
+	private scope: Scope = {};
 
-export interface NameGenerator {
-	(filePath?: string, scopeName?: string, className?: string, modName?: string): string;
-}
+	constructor(private generateName: (name: string, modName?: string) => string) {}
 
-export interface ExportsTaker {
-	(exports: Exports): void;
-}
-
-export interface PluginOptions {
-	generateName?: NameGenerator;
-	takeExports?: ExportsTaker;
-}
-
-const getName = (cacheKey: string, context: Context, generateName: () => string) => {
-	if (!context.cache[cacheKey]) {
-		context.cache[cacheKey] = generateName();
+	public processAtRule(atRule: AtRule): void {
+		if (atRule.name.match(KEYFRAMES_ATRULE_NAME_PATTERN)) {
+			atRule.params = this.processName(atRule.params);
+		}
 	}
 
-	return context.cache[cacheKey];
-};
+	public processRule(rule: Rule): void {
+		rule.selector = rule.selector.replace(CLASS_PATTERN, (match, ...matches) => {
+			return "." + this.processName(matches[0]);
+		});
 
-const processScope = (atRule: postcss.AtRule, context: Context, generateName: (scopeName: string, className: string, modName?: string) => string) => {
-	const scopeName = atRule.params;
+		rule.selector = rule.selector.replace(MOD_CLASS_PATTERN, (match, ...matches) => {
+			return "." + this.processName(matches[0], matches[2]);
+		});
 
-	let scope: Scope;
-
-	if (scopeName) {
-		context.exports.scopes[scopeName] = context.exports.scopes[scopeName] || {};
-		scope = context.exports.scopes[scopeName];
-	} else {
-		scope = context.exports.defaultScope;
+		rule.selector = rule.selector.replace(GLOBAL_CLASS_PATTERN, ".$1");
 	}
 
-	atRule.walkRules((atRule, index) => {
-		atRule.selector = atRule.selector.replace(CLASS_PATTERN, (match, ...matches) => {
-			const className = matches[0];
-			const cacheKey = [scopeName, className].join(".");
-
-			scope[className] = scope[className] || {};
-			scope[className].base = getName(cacheKey, context, () => {
-				return generateName(scopeName, className);
+	public processDeclaration(declaration: Declaration): void {
+		if (declaration.prop.match(ANIMATION_DECLARATION_NAME_PATTERN)) {
+			const names = declaration.value.split(/\s*,\s*/).map((value) => {
+				return value.replace(ANIMATION_DECLARATION_TOKEN_PATTERN, " ").trim();
 			});
 
-			return `.${scope[className].base}`;
-		});
+			declaration.value = names.reduce((result, name) => {
+				return result.replace(name, this.processName(name));
+			}, declaration.value);
+		}
+	}
 
-		atRule.selector = atRule.selector.replace(MOD_CLASS_PATTERN, (match, ...matches) => {
-			const className: string = matches[0];
-			const modName: string = matches[2];
-			const cacheKey = [scopeName, className, modName].join(".");
+	public getScope() {
+		return this.scope;
+	}
 
-			scope[className] = scope[className] || {};
-			scope[className].mods = scope[className].mods || {};
-			scope[className].mods[modName] = getName(cacheKey, context, () => {
-				return generateName(scopeName, className, modName);
-			});
+	private processName(name: string, modName?: string): string {
+		this.scope[name] = this.scope[name] || {
+			base: this.generateName(name),
+		};
 
-			return `.${scope[className].mods[modName]}`;
-		});
+		if (modName) {
+			this.scope[name].mods = this.scope[name].mods || {};
+			this.scope[name].mods[modName] = this.scope[name].mods[modName] || this.generateName(name, modName);
 
-		atRule.selector = atRule.selector.replace(GLOBAL_CLASS_PATTERN, ".$1");
-	});
+			return this.scope[name].mods[modName];
+		}
 
-	return atRule.nodes;
-};
-
-const generateDefaultName: NameGenerator = (filePath, scopeName, className, modName) => {
-	const fileName = filePath ? [...path.dirname(filePath).split(path.sep), path.basename(filePath, path.extname(filePath))] : [];
-
-	return [...fileName, scopeName, className, modName].join("_");
-};
-
-const dontTakeExports: ExportsTaker = (exports) => {};
+		return this.scope[name].base;
+	}
+}
 
 export default plugin<PluginOptions>(POSTCSS_PLUGIN, (pluginOptions = {}) => {
 	const generateName: NameGenerator = (typeof pluginOptions.generateName === "function") ? pluginOptions.generateName : generateDefaultName;
-	const takeExports: ExportsTaker = (typeof pluginOptions.takeExports === "function") ? pluginOptions.takeExports : dontTakeExports;
+	const takeScope: ScopeTaker = (typeof pluginOptions.takeScope === "function") ? pluginOptions.takeScope : dontTakeScope;
 
 	const transformer: Transformer = (root) => {
-		const context: Context = {
-			exports: {
-				scopes: {},
-				defaultScope: {},
-			},
+		const processor = new Processor((name: string, modName?: string): string => {
+			return generateName(path.relative(process.cwd(), root.source.input.file), name, modName);
+		});
 
-			cache: {},
-		};
+		root.walk((node) => {
+			switch (node.type) {
+				case "atrule":
+					processor.processAtRule(node);
+					break;
 
-		root.walkAtRules("scope", (atRule) => {
-			if (atRule.params.match(SCOPE_PATTERN)) {
-				atRule.replaceWith(...processScope(atRule, context, (scopeName, className, modName) => {
-					return generateName(path.relative(process.cwd(), root.source.input.file), scopeName, className, modName);
-				}));
+				case "rule":
+					processor.processRule(node);
+					break;
+
+				case "decl":
+					processor.processDeclaration(node);
+					break;
 			}
 		});
 
-		takeExports(context.exports);
+		takeScope(processor.getScope());
 	};
 
 	transformer.postcssPlugin = POSTCSS_PLUGIN;
